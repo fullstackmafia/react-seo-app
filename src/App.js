9cdc15e85b05ed7529c73c4671a3da6312e1ecd4
Raphael Ugwu <ugwuraphael@gmail.com>
2018-04-21 13:09:36 +0100

    Start with create-react-app

--- src/App.js
+++ src/App.js
@@ -2,28 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
-  constructor() {
-    super();
-    this.state = { repoNames: [] };
-  }
-
-  componentDidMount() {
-    let self = this;
-    fetch("https://api.github.com/repositories", {method: 'get'})
-      .then((response) => { return response.json(); })
-      .then((repos) => {
-        self.setState({ repoNames: repos.map((r) => { return r.name; })});
-      });
-
-    }
-
   render() {
     return (
-      <ol>
-        {this.state.repoNames.map((r, i) => { return <li key={i}>{r}</li> })}
-      </ol>
-    )
+     <div>
+        <h1>Googlebot will always crawl</h1>
+      </div>
+    );
   }
 }
 
